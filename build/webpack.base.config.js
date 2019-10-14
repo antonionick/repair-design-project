9937855397dfdd453424df71cfd78f429c3cbdec
Anton Golovko <golovko639@gmail.com>
2019-10-14 22:18:58 +0300

    fix: change webpack config, change behavior for browser support

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -4,88 +4,88 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const PATHS = {
-  src: path.join(__dirname, '../src'),
-  dist: path.join(__dirname, '../dist'),
-  assets: 'assets',
+	src: path.join(__dirname, '../src'),
+	dist: path.join(__dirname, '../dist'),
+	assets: 'assets',
 };
 
 module.exports = {
-  externals: {
-    paths: PATHS,
-  },
-  entry: {
-    app: PATHS.src,
-  },
-  output: {
-    filename: `[name].js`,
-    path: PATHS.dist,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            }
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              sourceMap: true,
-              config: {
-                path: 'postcss.config.js',
-              }
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              implementation: require('sass'),
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff(2)?|ttf|otf|eot)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-            }
-          }
-        ]
-      },
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: `[name].css`,
-    }),
-    new HtmlWebpackPlugin({
-      hash: false,
-      template: `${PATHS.src}/index.html`,
-      filename: './index.html',
-    }),
-    new CopyWebpackPlugin([
-      {from: `${PATHS.src}/${PATHS.assets}/img`, to: `${PATHS.dist}/${PATHS.assets}/img`},
-      {from: `${PATHS.src}/${PATHS.assets}/fonts`, to: `${PATHS.dist}/${PATHS.assets}/fonts`},
-    ]),
-  ],
+	externals: {
+		paths: PATHS,
+	},
+	entry: {
+		app: PATHS.src,
+	},
+	output: {
+		filename: `[name].js`,
+		path: PATHS.dist,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+						}
+					},
+					{
+						loader: "postcss-loader",
+						options: {
+							sourceMap: true,
+							config: {
+								path: 'postcss.config.js',
+							}
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true,
+							implementation: require('sass'),
+						},
+					},
+				],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: `${PATHS.assets}/img`
+						}
+					}
+				]
+			},
+			{
+				test: /\.(woff(2)?|ttf|otf|eot)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: `${PATHS.assets}/fonts`
+						}
+					}
+				]
+			},
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: `[name].css`,
+		}),
+		new HtmlWebpackPlugin({
+			template: `${PATHS.src}/index.html`,
+			filename: './index.html',
+		}),
+		new CopyWebpackPlugin([
+			{from: `${PATHS.src}/${PATHS.assets}/img`, to: `${PATHS.assets}/img`},
+		]),
+	],
 };
